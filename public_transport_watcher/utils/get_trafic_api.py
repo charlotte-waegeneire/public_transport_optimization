ef6e7d5f4d01fb45fd34ae0720fab19553c66999
Charlotte Waegeneire <charlotte.waegeneire@FRHQRMLT032.local>
2025-04-25 22:41:10 +0200

    logger instead of print

--- public_transport_watcher/utils/get_trafic_api.py
+++ public_transport_watcher/utils/get_trafic_api.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 import requests
 
+from public_transport_watcher.logging_config import get_logger
+
+logger = get_logger()
+
 load_dotenv()
 
 
@@ -14,5 +18,5 @@ def fetch_generic_api_data(url, headers=None, params=None, method="GET"):
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error during API request: {e}")
+        logger.error(f"Error during API request: {e}")
         return None
