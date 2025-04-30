8b1eac9d909bb91210be8d4baf1ca4565b0a87d6
Chloé <91722427+chloegaladriel@users.noreply.github.com>
2025-04-30 09:16:58 +0200

    [PM2-87] Add journey (#10)
    * add journey id migration
    
    * insert journey_id data
    
    * add changelog and version
    
    * ruff format

---
+++ public_transport_watcher/db/migrations/alembic/versions/79463770f866_added_journey_id.py
@@ -0,0 +1,29 @@
+"""added_journey_id
+
+Revision ID: 79463770f866
+Revises: a8ff31fc08d1
+Create Date: 2025-04-29 15:01:36.718648
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "79463770f866"
+down_revision: Union[str, None] = "a8ff31fc08d1"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column("schedule", sa.Column("journey_id", sa.String(), nullable=True), schema="transport")
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column("schedule", "journey_id", schema="transport")
