36f59d895344475bae5f76d2ef3b1e070736d69e
Charlotte <71834462+charlotte-waegeneire@users.noreply.github.com>
2025-05-09 20:44:03 +0200

    added calculation of walking trip during journey (#14)

--- public_transport_watcher/predictor/graph/__init__.py
+++ public_transport_watcher/predictor/graph/__init__.py
@@ -1,11 +1,13 @@
 from .calculate_travel_time import calculate_travel_time
 from .create_transport_network import create_transport_network
+from .find_nearest_station_with_walk import find_nearest_station_with_walk
 from .find_optimal_route import find_optimal_route
 from .visualize_network import visualize_network
 
 __all__ = [
     "calculate_travel_time",
     "create_transport_network",
+    "find_nearest_station_with_walk",
     "find_optimal_route",
     "visualize_network",
 ]
--- public_transport_watcher/predictor/graph_builder.py
+++ public_transport_watcher/predictor/graph_builder.py
@@ -2,14 +2,18 @@ import pickle
 
 import pandas as pd
 
+from public_transport_watcher.logging_config import get_logger
 from public_transport_watcher.predictor.configuration import PREDICTION_CONFIG
 from public_transport_watcher.predictor.graph import (
     calculate_travel_time,
     create_transport_network,
+    find_nearest_station_with_walk,
     find_optimal_route,
     visualize_network,
 )
 from public_transport_watcher.utils import get_engine
+
+logger = get_logger()
 
 
 class GraphBuilder:
@@ -28,6 +32,7 @@ class GraphBuilder:
         G = self.build_graph()
         network_path = self.graph_config.get("network_path", "")
         if not network_path:
+            logger.error("Network path is not set")
             raise ValueError("Network path is not set")
         with open(network_path, "wb") as f:
             pickle.dump(G, f)
@@ -35,6 +40,7 @@ class GraphBuilder:
     def load_graph(self) -> str:
         network_path = self.graph_config.get("network_path", "")
         if not network_path:
+            logger.error("Network path is not set")
             raise ValueError("Network path is not set")
         with open(network_path, "rb") as f:
             return pickle.load(f)
@@ -43,13 +49,44 @@ class GraphBuilder:
         G = self.load_graph()
         return visualize_network(G)
 
-    def find_optimal_route(self, start_station_id: int, end_station_id: int) -> str:
+    def find_optimal_route(self, start_coords: tuple, end_coords: tuple) -> str:
         G = self.load_graph()
-        return find_optimal_route(G, start_station_id, end_station_id)
+        (start_lat, start_lon) = start_coords
+        (end_lat, end_lon) = end_coords
+
+        start_station = find_nearest_station_with_walk(start_lat, start_lon, G)
+        end_station = find_nearest_station_with_walk(end_lat, end_lon, G)
+
+        if not start_station or not end_station:
+            logger.error("Impossible to find a starting or ending station")
+            raise ValueError("Impossible to find a starting or ending station")
+
+        walking_distance_start = start_station["walking_distance"]
+        walking_duration_start = start_station["walking_duration"]
+
+        walking_distance_end = end_station["walking_distance"]
+        walking_duration_end = end_station["walking_duration"]
+
+        optimal_path, network_time, route_info = find_optimal_route(
+            G, start_station["station_id"], end_station["station_id"]
+        )
+
+        total_walking_distance = walking_distance_start + walking_distance_end
+        total_walking_duration = walking_duration_start + walking_duration_end
+        total_time = total_walking_duration + network_time
+
+        return {
+            "walking_distance": total_walking_distance,
+            "walking_duration": total_walking_duration,
+            "network_time": network_time,
+            "total_time": total_time,
+            "optimal_path": optimal_path,
+            "route_info": route_info,
+        }
 
 
 if __name__ == "__main__":
     graph_builder = GraphBuilder()
     graph_builder.save_graph()
     graph = graph_builder.load_graph()
-    optimal_path, total_time, route_info = graph_builder.find_optimal_route(1, 2)
+    route_info = graph_builder.find_optimal_route((48.8566, 2.3522), (48.8637, 2.3488))
