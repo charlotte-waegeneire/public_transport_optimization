5031eb79d082245cafcca507ad9fa00e95011c4b
Charlotte <charlotte.waegeneire@hotmail.fr>
2025-05-22 21:48:11 +0200

    dead code

--- public_transport_watcher/db/models/user.py
+++ public_transport_watcher/db/models/user.py
@@ -1,18 +1,13 @@
 from sqlalchemy import Column, DateTime, Integer, String, text
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 users_schema = "application"
 
 
-class User:
+class User(Base):
     __tablename__ = "users"
     __table_args__ = {"schema": users_schema}
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), nullable=False, unique=True)
-    password_hash = Column(String(255), nullable=False)
-    email = Column(String(100), nullable=False, unique=True)
-    created_at = Column(DateTime, server_default=text("NOW()"), nullable=False)
-    updated_at = Column(DateTime, server_default=text("NOW()"), nullable=False)
 
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False, unique=True)
--- public_transport_watcher/utils/query/get_validations_per_month_year.sql
+++ public_transport_watcher/utils/query/get_validations_per_month_year.sql
@@ -1,21 +1,3 @@
--- SELECT
---     EXTRACT(YEAR FROM start_timestamp) AS year
---     , EXTRACT(MONTH FROM start_timestamp) AS month
---     , COUNT(*) AS count
--- FROM
---     transport.traffic t
--- JOIN
---     transport.time_bin tb
--- ON
---     t.time_bin_id = tb.id
--- GROUP BY
---     year,
---     month
--- ORDER BY
---     year,
---     month;
-
--- Vue pour optimiser les performances
 CREATE VIEW transport.station_transport_mapping AS
 SELECT DISTINCT
     station_id,
